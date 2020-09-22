ba7d2ab81b79ba2475d3d2fe68a7686edd61efab
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-22 11:10:06 +0530

    Update ContactService.java

--- src/main/java/com/wm/service/ContactService.java
+++ src/main/java/com/wm/service/ContactService.java
@@ -3,12 +3,14 @@ package com.wm.service;
 import com.wm.model.Contact;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ContactService {
 
     Contact createContact(Contact person);
 
     public List<Contact> getAllContacts();
+
     long countContacts();
     public Optional<Contact> getContactById(Long Id);
 }
